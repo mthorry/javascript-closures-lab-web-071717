90c15006d829985c51282488f8252a97a25f804b
mthorry <mthorry@gmail.com>
2017-08-29 13:04:52 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 const app = "I don't do much."
+
+function bumpCounter() {
+  let counter = 0;
+
+  function addBump() {
+    counter += 1
+  }
+
+  function getBumps() {
+    return counter
+  }
+
+  return {
+    addBump,
+    getBumps,
+  };
+
+}
+
+function createAnimal(animalType) {
+  return function (deadlyDevice) {
+    return {deadlyDevice, animalType}
+  }
+}
+
+var sharkCreator = createAnimal('Shark');
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam');
+var sharkWithFrickinCannon = sharkCreator('Cannon');
